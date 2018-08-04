b684268270d5124a1fec7f7af16369ef5ed21081
Javier Vieira <xavi.vieira@gmail.com>
2018-08-04 22:11:35 +0200

    basic drawer activity

--- app/src/main/java/unxavi/com/github/project404/features/splash/SplashActivity.java
+++ app/src/main/java/unxavi/com/github/project404/features/splash/SplashActivity.java
@@ -3,10 +3,18 @@ package unxavi.com.github.project404.features.splash;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import unxavi.com.github.project404.features.main.MainActivity;
+
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        MainActivity.start(this);
     }
 }
