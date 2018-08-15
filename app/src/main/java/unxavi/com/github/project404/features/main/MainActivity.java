7b72af1b0d708c4fbbdfbb314868dfbf6dc322c4
Javier Vieira <javier.vieira@qustodio.com>
2018-08-15 22:23:37 +0200

    drawer menu for the app

--- app/src/main/java/unxavi/com/github/project404/features/main/MainActivity.java
+++ app/src/main/java/unxavi/com/github/project404/features/main/MainActivity.java
@@ -147,17 +147,11 @@ public class MainActivity extends MvpActivity<MainActivityView, MainActivityPres
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.signin) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.tasks) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.signout) {
 
         }
 
