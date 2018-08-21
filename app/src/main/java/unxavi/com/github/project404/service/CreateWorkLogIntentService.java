134b54f74008af8fe12c05b46b928d126d4974ba
Javier Vieira <javier.vieira@qustodio.com>
2018-08-21 08:28:05 +0200

    get location from widget and create the work log

--- app/src/main/java/unxavi/com/github/project404/service/CreateWorkLogIntentService.java
+++ app/src/main/java/unxavi/com/github/project404/service/CreateWorkLogIntentService.java
@@ -3,9 +3,12 @@ package unxavi.com.github.project404.service;
 import android.app.IntentService;
 import android.appwidget.AppWidgetManager;
 import android.content.ComponentName;
-import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.text.TextUtils;
 
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -13,6 +16,8 @@ import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import io.nlopez.smartlocation.OnLocationUpdatedListener;
+import io.nlopez.smartlocation.SmartLocation;
 import timber.log.Timber;
 import unxavi.com.github.project404.data.FirestoreHelper;
 import unxavi.com.github.project404.model.WorkLog;
@@ -61,6 +66,24 @@ public class CreateWorkLogIntentService extends IntentService implements Firesto
     }
 
     private void createWorkLog(final int workLogAction) {
+        if (Build.VERSION.SDK_INT >= 23 &&
+                ContextCompat.checkSelfPermission(
+                        getApplicationContext(),
+                        android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            SmartLocation.with(getApplicationContext()).location()
+                    .oneFix()
+                    .start(new OnLocationUpdatedListener() {
+                        @Override
+                        public void onLocationUpdated(Location location) {
+                            createWorkLogWithLocation(workLogAction, location);
+                        }
+                    });
+        }else{
+            createWorkLogWithLocation(workLogAction, null);
+        }
+    }
+
+    private void createWorkLogWithLocation(final int workLogAction, final Location location) {
         Query lastUserWorkLog = FirestoreHelper.getInstance().getLastUserWorkLog();
         if (lastUserWorkLog != null) {
             lastUserWorkLog.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
@@ -75,9 +98,8 @@ public class CreateWorkLogIntentService extends IntentService implements Firesto
                                 Timber.e(exception);
                             }
                         }
-                        // TODO: 20/08/2018 location
                         if (lastWorkLog != null) {
-                            WorkLog workLog = new WorkLog(workLogAction, lastWorkLog.getTask(), null);
+                            WorkLog workLog = new WorkLog(workLogAction, lastWorkLog.getTask(), location);
                             FirestoreHelper.getInstance().addUserWorkLog(workLog, CreateWorkLogIntentService.this);
                         }
                     } else {
