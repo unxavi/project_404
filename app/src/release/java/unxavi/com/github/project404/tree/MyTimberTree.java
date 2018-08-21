f131b13c7e2ff898951edbfb7e728754c19c84ea
Javier Vieira <javier.vieira@qustodio.com>
2018-08-21 08:57:19 +0200

    add crashlitycs

--- app/src/release/java/unxavi/com/github/project404/tree/MyTimberTree.java
+++ app/src/release/java/unxavi/com/github/project404/tree/MyTimberTree.java
@@ -3,6 +3,8 @@ package unxavi.com.github.project404.tree;
 import android.support.annotation.NonNull;
 import android.util.Log;
 
+import com.crashlytics.android.Crashlytics;
+
 import timber.log.Timber;
 
 
@@ -10,12 +12,10 @@ public class MyTimberTree extends Timber.Tree {
     @Override
     protected void log(int priority, String tag, @NonNull String message, Throwable t) {
 
-        if (priority == Log.WARN) {
-            // TODO: 8/4/18   YourCrashLibrkary.logWarning(throwable);
-        } else if (priority == Log.ERROR) {
-            // TODO: 8/4/18   YourCrashLibrary.logException(throwable);
+        if (priority == Log.WARN || priority == Log.ERROR) {
+            Crashlytics.logException(t);
         }else{
-            //discard the logs
+            //discard the logs for production
         }
     }
 }
